29d5151b767fed92bee24d38705ac67bd76b4fb8
Ashwath29 <78354532+Ashwath29@users.noreply.github.com>
2022-10-25 16:13:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function adduser() {
+    window.location = "bedroom.html";
+}
+
+function adduser1() {
+    window.location = "TV and AC room.html";
+}
+
+function adduser2() {
+    window.location = "deskroom.html";
+}
